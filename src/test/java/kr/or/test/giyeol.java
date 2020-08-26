107b0f99cae75128209cdfde105b1ccca7b7f77d
hu-24 <hu-24@hu-24-PC>
2020-08-26 16:33:36 +0900

    mysql 프로젝트 싱크

---
+++ src/test/java/kr/or/test/giyeol.java
@@ -0,0 +1,15 @@
+package kr.or.test;
+
+public class giyeol {
+
+	public static void main(String[] args) {
+		for(int i = 1; i<5; i++) {
+			for(int z = i*2; z<10; z++)
+				System.out.println("*");
+			}
+			
+		
+
+	}
+
+}
